bd2ac54103601aaa2bfe50bbcd36e9a929046ad9
James Wasmer <jameswasmer@gmail.com>
2022-10-11 16:57:38 -0400

    Initial commit

---
+++ src/Game.js
@@ -0,0 +1,5 @@
+class BigPicture {
+  constructor(players) {
+    
+  }
+}
---
+++ test/game-test.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+var Game = require('../src/Game');
+var Player = require('../src/Player');
+
+describe('Game', () => {
+  let game;
+  it.skip('should accept an array of Player class instances', () => {
+    game = new Game();
+  })
+})
